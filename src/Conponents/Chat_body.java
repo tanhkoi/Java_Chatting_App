b982b8cc46c302c849f78f3ef6f108fafaf85a3c
Nguyen Tan Khoi <khoitony35@gmail.com>
2023-10-21 20:15:34 +0700

    Update

--- src/Conponents/Chat_body.java
+++ src/Conponents/Chat_body.java
@@ -13,8 +13,8 @@ public class Chat_body extends javax.swing.JPanel {
         init();
 //        addItemLeft("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", "Lorem");
 //        addItemLeft("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", "Lorem");
-        addItemLeft("Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "Lorem", new ImageIcon(getClass().getResource("/image/Nen.jpg"))); //, new ImageIcon(getClass().getResource("/image/Nen.jpg"))
-        addItemFile("", "Lorem", "my doc.pdf", "1 MB");
+        // addItemLeft("Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "Lorem", new ImageIcon(getClass().getResource("/image/Nen.jpg"))); //, new ImageIcon(getClass().getResource("/image/Nen.jpg"))
+        //addItemFile("", "Lorem", "my doc.pdf", "1 MB");
     }
 
     private void init() {
